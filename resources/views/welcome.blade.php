92baea6731d29f8390647788afe8fc286d5478da
musadiq90 <musadiq.iba@gmail.com>
2019-04-27 12:32:53 -0700

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Foodex
                 </div>
 
                 <div class="links">
